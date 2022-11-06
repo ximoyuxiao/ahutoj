a056e47ef4e9936a310901ef2e3d964cdd81e44f
LanQiDongLai <i1901433058@163.com>
2022-11-06 22:59:33 +0800

    add:判题机天假python支持

---
+++ core/test/judgeClient_test.cpp
@@ -0,0 +1,239 @@
+#include "../mlog.h"
+#include "../Solve.h"
+#include "../judgeClient.h"
+#include "../result.h"
+#include "../readConfig.h"
+using namespace my;
+int allPass = 0;
+int all = 0;
+SubRes status;
+
+static void judgeClient_AC(){
+    all++;
+    printf("Test AC:");
+    Solve* solve = nullptr;
+    solve =  new Solve(1,1,"#include<iostream>\nusing namespace std;\nint main(){\n\tcout<<\"hello world\";\n\texit(0);\n}",1000,128,CPP11);
+    judgeClient juc(solve);
+    status = OJ_JUDGE;
+    juc.judge();
+    if(solve->Sres() != OJ_AC){
+        printf("Failed res=%s\n",runningres[solve->Sres()]);
+        return ;
+    }
+    puts("pass");
+    allPass++;
+}
+
+static void judgeClient_PE(){
+    all++;
+    printf("Test PE:");
+    Solve* solve = nullptr;
+    solve  = new Solve(1,1,"#include<iostream>\nusing namespace std;\nint main(){\n\tcout<<\"hello world\"<<endl<<endl;\n\texit(0);\n}",1000,128,CPP11);
+    judgeClient juc(solve);
+    status = OJ_JUDGE;
+    juc.judge();
+    if(solve->Sres() != OJ_PE){
+        printf("Failed res=%s\n",runningres[solve->Sres()]);
+        return ;
+    }
+    puts("pass");
+    allPass++;
+    delete solve;
+}
+
+static void judgeClient_RE(){
+    all++;
+    printf("Test RE:");
+    Solve* solve = nullptr;
+    solve =  new Solve(1,1,"#include<iostream>\nusing namespace std;\nint main(){\n\tint a=1,b=0;\n\tcout<<a/b;\n\texit(0);\n}",1000,128,CPP11);
+    judgeClient juc(solve);
+    status = OJ_JUDGE;
+    juc.judge();
+    if(solve->Sres() != OJ_RE){
+        printf("Failed res=%s\n",runningres[solve->Sres()]);
+        return ;
+    }
+    puts("pass");
+    allPass++;
+}
+
+static void judgeClient_WA(){
+     all++;
+    printf("Test WA:");
+    Solve* solve = nullptr;
+    solve =  new Solve(1,1,"#include<iostream>\nusing namespace std;\nint main(){\n\tcout<<\"world\";\n\texit(0);\n}",1000,128,CPP11);
+    judgeClient juc(solve);
+    status = OJ_JUDGE;
+    juc.judge();
+    if(solve->Sres() != OJ_WA){
+        printf("Failed res=%s\n",runningres[solve->Sres()]);
+        return ;
+    }
+    puts("pass");
+    allPass++;
+}
+
+static void judgeClient_TLE(){
+    all++;
+    printf("Test TLE:");
+    Solve *solve;
+    solve = new Solve(1,1,"#include<iostream>\nusing namespace std;\nint main(){\n\tfor(int i=0;;i++){int a =0;}\n\texit(0);\n}",1000,128,CPP11);
+    judgeClient juc(solve);
+    status = OJ_JUDGE;
+    juc.judge();
+    if(solve->Sres() != OJ_TLE){
+        printf("Failed res=%s\n",runningres[solve->Sres()]);
+        return ;
+    }
+    puts("pass");
+    allPass++;
+}
+
+static void judgeClient_MLE(){
+     all++;
+    printf("Test MLE:");
+    Solve* solve = nullptr;
+    solve =  new Solve(1,1,"#include<iostream>\n#include<cstdlib>\nusing namespace std;\nint main(){\n\tauto arr= malloc(256*1024*1024);\n\texit(0);\n}",1000,128,CPP11);
+    judgeClient juc(solve);
+    status = OJ_JUDGE;
+    juc.judge();
+    if(solve->Sres() != OJ_MLE){
+        printf("Failed res=%s\n",runningres[solve->Sres()]);
+        return ;
+    }
+    puts("pass");
+    allPass++;
+}
+
+static void judgeClient_CPP_test()
+{
+    judgeClient_AC();
+    judgeClient_TLE();
+    judgeClient_MLE();
+    judgeClient_PE();
+    judgeClient_RE();
+    judgeClient_WA();
+    printf("you all test:%d,you pass:%d\nyou pass rate:%.2lf%%\n",all,allPass,1.0*allPass/all*100);
+}
+
+
+static void judgeClient_PY3_AC(){
+    all++;
+    printf("Test AC:");
+    Solve* solve = nullptr;
+    solve =  new Solve(1,1,"a=input()\nls=a.split()\nprint('hello world')",1000,128,PYTHON3);
+    judgeClient juc(solve);
+    status = OJ_JUDGE;
+    juc.judge();
+    if(solve->Sres() != OJ_AC){
+        printf("Failed res=%s\n",runningres[solve->Sres()]);
+        return ;
+    }
+    puts("pass");
+    allPass++;
+}
+
+static void judgeClient_PY3_PE(){
+    all++;
+    printf("Test PE:");
+    Solve* solve = nullptr;
+    solve  = new Solve(1,1,"print(\"\"\"\nhello world\"\"\")",1000,128,PYTHON3);
+    judgeClient juc(solve);
+    status = OJ_JUDGE;
+    juc.judge();
+    if(solve->Sres() != OJ_PE){
+        printf("Failed res=%s\n",runningres[solve->Sres()]);
+        return ;
+    }
+    puts("pass");
+    allPass++;
+    delete solve;
+}
+
+static void judgeClient_PY3_RE(){
+    all++;
+    printf("Test RE:");
+    Solve* solve = nullptr;
+    solve =  new Solve(1,1,"1/0",1000,128,PYTHON3);
+    judgeClient juc(solve);
+    status = OJ_JUDGE;
+    juc.judge();
+    if(solve->Sres() != OJ_RE){
+        printf("Failed res=%s\n",runningres[solve->Sres()]);
+        return ;
+    }
+    puts("pass");
+    allPass++;
+}
+
+static void judgeClient_PY3_WA(){
+     all++;
+    printf("Test WA:");
+    Solve* solve = nullptr;
+    solve =  new Solve(1,1,"print(\'2345\')",1000,128,PYTHON3);
+    judgeClient juc(solve);
+    status = OJ_JUDGE;
+    juc.judge();
+    if(solve->Sres() != OJ_WA){
+        printf("Failed res=%s\n",runningres[solve->Sres()]);
+        return ;
+    }
+    puts("pass");
+    allPass++;
+}
+
+static void judgeClient_PY3_TLE(){
+    all++;
+    printf("Test TLE:");
+    Solve *solve;
+    solve = new Solve(1,1,"while True:\n\tpass",1000,128,PYTHON3);
+    judgeClient juc(solve);
+    status = OJ_JUDGE;
+    juc.judge();
+    if(solve->Sres() != OJ_TLE){
+        printf("Failed res=%s\n",runningres[solve->Sres()]);
+        return ;
+    }
+    puts("pass");
+    allPass++;
+}
+
+static void judgeClient_PY3_MLE(){
+     all++;
+    printf("Test MLE:");
+    Solve* solve = nullptr;
+    solve =  new Solve(1,1,"ls = [None] * 1024 * 1024 * 1024",1000,128,PYTHON3);
+    judgeClient juc(solve);
+    status = OJ_JUDGE;
+    juc.judge();
+    if(solve->Sres() != OJ_MLE){
+        printf("Failed res=%s\n",runningres[solve->Sres()]);
+        return ;
+    }
+    puts("pass");
+    allPass++;
+}
+
+
+static void judgeClient_PY3_test()
+{
+    allPass = 0;
+    all = 0;
+    judgeClient_PY3_AC();
+    judgeClient_PY3_TLE();
+    judgeClient_PY3_MLE();
+    judgeClient_PY3_PE();
+    judgeClient_PY3_RE();
+    judgeClient_PY3_WA();
+    printf("you all py3 test:%d,you pass:%d\nyou pass rate:%.2lf%%\n",all,allPass,1.0*allPass/all*100);
+}
+
+int main()
+{
+    mlog::init("./log");
+    judgeClient_CPP_test();
+    judgeClient_PY3_test();
+    mlog::destory();
+    printf("exit!\n");
+    return 0;
+}
--- doc/oj.sql
+++ doc/oj.sql
@@ -158,7 +158,7 @@ create table CEINFO(
 
 ALTER TABLE Submit AUTO_INCREMENT = 1000;
 #5.添加数据
--- insert into User values('admin','墨羽','21de184f26d37d33d5581d923ae52c17','AHUT','软191','软件工程',null,null,'a2571717957@163.com','动态规划','');             
+insert into User values('admin','墨羽','21de184f26d37d33d5581d923ae52c17','AHUT','软191','软件工程',null,null,'a2571717957@163.com','动态规划','');             
 insert into Permission values('admin','Y','Y','Y','Y','Y');
 insert into Problem values(null,'A+B问题','输入一个数字A和一个数字B要求输出A和B的和','分别输入两个整数A和B','输出A和B的和','1 2','3','1','128','','基础',0,0);
 insert into Contest values(null,'admin','测试比赛1','用于测试',1639559000000,1639599000000,1,1,"1000,",null);
