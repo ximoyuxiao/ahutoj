7a5a5efb530959a4557379dde8b8316fe77af04a
xiaomoyu <a2571717957@163.com>
2023-05-03 11:38:48 +0800

    add:增加运行和关闭的两个脚本

--- run.sh
+++ run.sh
@@ -1,3 +1,19 @@
-cd tmp/bin
-nohup ./gatway ../config/config.yaml &
-nohup ./ahutoj ../config/config.yaml &
+cd ./tmp/bin
+
+run(){
+    if [ ! -d $1 ]; then \
+		mkdir -p $1; \
+	fi
+    cd $1
+    nohup ../$1 ../../config/config.yaml &
+    cd ..
+}
+
+for exec in `gatway ahutoj originJudge persistence`;
+do
+    if test -x $i;then
+        run exec
+    fi;
+done
+cd core
+./jugded
