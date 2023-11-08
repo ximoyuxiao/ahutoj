6b79be684fe7a359c9832b611acc5277dcf9e57e
fangruijia <2648242688@qq.com>
2023-11-08 14:29:42 +0800

    fix:我眼睛瞎了，每次我以为解决了然而总是伴随了另外的问题！

--- core/Solution.cpp
+++ core/Solution.cpp
@@ -184,9 +184,9 @@ bool Solution::commitSolveToDb(Solve* solve){
         runningres[solve->Sres()],
         solve->getUsetime(),
         solve->getuseMemory(),
-        solve->Sid(),
         solve->getPassSample(),
-        solve->getSampleNumber()
+        solve->getSampleNumber(),
+        solve->Sid()
     );
     ILOG("update mysql:%s",sql);
     auto db = mysqlDB::getInstance();
