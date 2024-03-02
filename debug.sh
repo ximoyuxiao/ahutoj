11355ef8f300775ae2910432bd1a369dd868ada1
fangruijia <2648242688@qq.com>
2024-03-02 22:05:18 +0800

    fix And add:测试一下新增的日志系统，修改了一些路径

---
+++ debug.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Array of Docker containers
+declare -a containers=( "oj-judged" "oj-origin" "oj-persistence" "oj-oj")
+
+# Array of Docker images
+declare -a images=( "oj/judged:local" "oj/origin:local" "oj/persistence:local" "oj/oj:local")
+
+# Stop Docker containers
+for container in "${containers[@]}"
+do
+   echo "Stopping $container"
+   sudo docker stop $container
+done
+
+# Remove Docker images
+for image in "${images[@]}"
+do
+   echo "Removing $image"
+   sudo docker rmi -f $image
+done
+
+sudo docker-compose up -d
+
+echo "Script completed"
+
